0a390234f535bd9c273f673819aa0f0a5528659a
Rishi9398 <rishithreddyv143@gmail.com>
2024-12-05 16:14:36 +0530

    ba

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const connectDB = require('./config/db');
 const errorHandler = require('./config/errorHandler');
 const tasksRouter = require('./routers/tasksRouter');
-const sosRouter = require('./routers/sosRouter'); // Add this if you need SOS routes
+
 const authRouter = require('./routers/authRouter'); // Add authRouter for login and registration
 
 
@@ -13,8 +13,8 @@ connectDB();
 
 const app = express();
 
-// Middleware
-app.use(cors()); // Enable CORS for cross-origin requests
+
+app.use(cors()); 
 app.use(express.json()); // Parse incoming JSON requests
 app.use(errorHandler); // Custom error handler middleware
 
